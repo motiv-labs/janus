0f2813fd4b731f1c2a9b210e033101b0d6ce7cce
Italo Lelis de Vietro <italolelis@gmail.com>
2016-07-15 11:40:15 +0200

    Moved config to it's own file

---
+++ config.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/hellofresh/api-gateway/storage"
+)
+
+// Specification for basic configurations
+type Specification struct {
+	storage.Database
+	Port  int                 `envconfig:"PORT"`
+	Debug bool                `envconfig:"DEBUG"`
+	Storage
+}
+
+type Storage struct {
+	DSN      string            `envconfig:"REDIS_DSN"`
+	Password string            `envconfig:"REDIS_PASSWORD"`
+	Database int64             `envconfig:"REDIS_DB"`
+}
