f7de0a68f7235bb051e06848cc100f71b299115e
Italo Lelis de Vietro <italolelis@gmail.com>
2016-07-22 22:46:22 +0200

    Fixed session state ison definition

--- session_state.go
+++ session_state.go
@@ -1,9 +1,16 @@
 package main
 
+// Enums for keys to be stored in a session context - this is how gorilla expects
+// these to be implemented and is lifted pretty much from docs
+const (
+	SessionData = "session_data"
+	AuthHeaderValue = "auth_header"
+)
+
 // SessionState objects represent a current API session, mainly used for rate limiting.
 type SessionState struct {
 	ExpiresIn     int64                       `json:"expires_in"`
 	OauthClientID string                      `json:"oauth_client_id"`
-	AccessToken   string                      `json:"client_id"`
+	AccessToken   string                      `json:"access_token"`
 	TokenType     string                      `json:"token_type"`
 }
