ab87dcc2f3c8a1d7641ca008c90067edf4cffdce
Italo Lelis de Vietro <italolelis@gmail.com>
2016-07-22 22:47:39 +0200

    Checking auth for proper key

--- oauth_manager.go
+++ oauth_manager.go
@@ -4,38 +4,37 @@ import (
 	"gopkg.in/redis.v3"
 	"encoding/json"
 	log "github.com/Sirupsen/logrus"
+	"time"
 )
 
 type OAuthManager struct {
 	storage *redis.Client
 }
 
-func NewOAuthManager(client *redis.Client) *OAuthManager {
-	return &OAuthManager{client}
+func (o OAuthManager) KeyExists(accessToken string) bool {
+	log.Debugf("Searching for key %s", accessToken)
+	return o.storage.Exists(accessToken).Val()
 }
 
-func (o OAuthManager) CheckSessionAndIdentityForValidKey(accessToken string) (SessionState, bool) {
+func (o OAuthManager) Set(accessToken string, session SessionState, resetTTLTo int64) error {
+	value, _ := json.Marshal(session)
+	expireDuration := time.Duration(resetTTLTo)
+	expireTime := expireDuration * time.Second
+
+	log.Debugf("Storing key %s for %d seconds", accessToken, expireTime)
+	go o.storage.Set(accessToken, string(value), expireDuration)
+	return nil
+}
+
+func (o OAuthManager) IsKeyAuthorised(accessToken string) (SessionState, bool) {
 	var newSession SessionState
+	jsonKeyVal := o.storage.Get(accessToken).Val()
 
-	//Checks if the key is present on the cache and if it didn't expire yet
-	if o.storage.Exists(accessToken).Val() {
-		jsonKeyVal := o.storage.Get(accessToken).String()
-
-		if marshalErr := json.Unmarshal([]byte(jsonKeyVal), &newSession); marshalErr != nil {
-			log.Error("Couldn't unmarshal session object")
-			log.Error(marshalErr)
-			return newSession, false
-		}
-
-		return newSession, true
+	if marshalErr := json.Unmarshal([]byte(jsonKeyVal), &newSession); marshalErr != nil {
+		log.Error("Couldn't unmarshal session object")
+		log.Error(marshalErr)
+		return newSession, false
 	}
-
-
-	//if its not in the cache
-	//make a request to /info on the auth service
-	//if the key is invalid return nil and false
-	//otherwise save the key on the cache
-	//and return the session and true
 
 	return newSession, true
 }
