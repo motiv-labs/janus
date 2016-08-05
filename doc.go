2dcf5041c663bec03f54fd047bd4ca44924d56ea
Italo Lelis de Vietro <italolelis@gmail.com>
2016-08-05 15:09:56 +0200

    Added doc go

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+This is a lightweight, API Gateway and Management Platform enables you to control who accesses your API,
+when they access it and how they access it. API Gateway will also record detailed analytics on how your
+users are interacting with your API and when things go wrong.
+
+For a full guide visit https://github.com/hellofresh/api-gateway
+*/
+package main
