5735dfbe552a9eecb64bea0aed0e45568957f453
Italo Lelis de Vietro <italolelis@gmail.com>
2016-09-24 15:46:59 +0200

    Added test suit

---
+++ api_gateway_suite_test.go
@@ -0,0 +1,13 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestApiGateway(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ApiGateway Suite")
+}
