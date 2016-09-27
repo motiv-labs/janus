195ec9167ca9933af777ef6c8d0a64ba983ba0e3
Kieran <me@kieranajp.co.uk>
2016-09-27 13:54:56 +0200

    CORS that may or may not be working...

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
+	"github.com/itsjamie/gin-cors"
 	"github.com/kelseyhightower/envconfig"
 	"gopkg.in/redis.v3"
 )
@@ -68,6 +69,16 @@ func main() {
 		log.SetLevel(log.InfoLevel)
 		gin.SetMode(gin.ReleaseMode)
 	}
+
+	// Set up CORS
+	router.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Authorization, Content-Type",
+		ExposedHeaders:  "",
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
 
 	accessor := initializeDatabase()
 	defer accessor.Close()
