f8b319788b10c2deafcfd0fac5907da363e74420
Italo Lelis de Vietro <italolelis@gmail.com>
2016-09-28 22:13:23 +0200

    Better docs of the build

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
-set -x
-
 CWD=$(pwd)
+
+# Defines the zip name
 DEST=api-gateway
 
+# Goes to the application source code
+cd api-gateway-master
+
+# Creates the project source on the gopath
 mkdir -p ${PROJECT_SRC}
-cp -r api-gateway-master/ ${PROJECT_SRC}
+
+# Copies the current srouce code from the app to the gopath
+cp -r . ${PROJECT_SRC}
+
+# Goes to the application on the gopath
 cd ${PROJECT_SRC}
 
-# build go binary
+# Build the go application
 make
 
+# Goes to the generated go binaries
 cd /go/bin
 
+# Zip all binaries in one sigle tar
 echo "* Creating tar.gz"
 tar -czvf ${DEST}.tar.gz ${DEST}
 
-echo "* copying ${DEST}.tar.gz ${CWD}/artifacts"
-cp ${DEST}.tar.gz ${CWD}/artifacts
+# Copies the tar to the artifact folder so its available to the next step of the pipeline
+echo "* Copying *.tar.gz ${CWD}/artifacts"
+cp *.tar.gz ${CWD}/artifacts
