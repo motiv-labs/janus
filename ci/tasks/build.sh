88384a52060eb33cc2ec40601c3d876c7dd23a6d
Italo Lelis de Vietro <italolelis@gmail.com>
2016-09-28 16:36:39 +0200

    Chaged build script

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 
 CWD=$(pwd)
 DEST=api-gateway
@@ -6,12 +7,11 @@ DEST=api-gateway
 mkdir -p ${PROJECT_SRC}
 cp -r . ${PROJECT_SRC}
 cd ${PROJECT_SRC}
-
+#build go binary
 make
 
 cd /go/bin
-
 echo "* Creating tar.gz"
 tar -czf ${DEST}.tar.gz ${DEST} > /dev/null
-
+echo "* copying ${DEST}.tar.gz ${CWD}/artifacts"
 cp ${DEST}.tar.gz ${CWD}/artifacts
