fecce6141b94ca523c1ae220e35be8eac4b09976
Italo Lelis de Vietro <italolelis@gmail.com>
2016-11-11 14:55:56 +0100

    New pullrequest pipeline

---
+++ ci/scripts/run-unit-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Creates the project source on the gopath
+mkdir -p ${PROJECT_SRC}
+
+# Copies the current srouce code from the app to the gopath
+cp -r . ${PROJECT_SRC}
+
+# Goes to the application on the gopath
+cd ${PROJECT_SRC}
+
+make deps
+make test
--- ci/scripts/unit-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ${PROJECT_SRC}
-cp -r . ${PROJECT_SRC}
-cd ${PROJECT_SRC}
-
-pwd
-make deps
-make test
