89936e26d0ef1a0a1ddbf18642a863585df02474
Italo Lelis de Vietro <italolelis@gmail.com>
2016-11-15 16:08:11 +0100

    Added the docker flow

---
+++ ci/scripts/docker-deployment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Ensure this script fails if anything errors
+set -e
+
+# Creates the necessary directories
+mkdir -p docker-images/dev
+mkdir -p docker-images/latest
+
+# Copies code to the image directories
+cp -r source-code/* docker-images/dev
+cp -r source-code/* docker-images/latest
+
+# Copies code to the image directories
+cp docker-images/dev/ci/assets/Dockerfile.dev docker-images/dev/Dockerfile
+cp docker-images/latest/ci/assets/Dockerfile docker-images/dev/Dockerfile 
