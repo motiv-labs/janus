05a59905d2ce6111506dce3b3b8072cd6997aece
Italo Lelis de Vietro <italolelis@gmail.com>
2016-11-15 16:28:12 +0100

    changed how to fetch dev version

--- ci/scripts/docker-deployment.sh
+++ ci/scripts/docker-deployment.sh
@@ -8,9 +8,10 @@ mkdir -p docker-images/dev
 mkdir -p docker-images/latest
 
 # Copies code to the image directories
-cp -r source-code/* docker-images/dev
-cp -r source-code/* docker-images/latest
+cp -r source-code docker-images/dev
+cp -r source-code docker-images/latest
 
 # Copies code to the image directories
 cp docker-images/dev/ci/assets/Dockerfile.dev docker-images/dev/Dockerfile
+echo "dev" >> docker-images/dev/version
 cp docker-images/latest/ci/assets/Dockerfile docker-images/dev/Dockerfile 
