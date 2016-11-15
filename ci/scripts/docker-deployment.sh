7284b38b5e4938eea373ccc16ddc10ef81ea9641
Italo Lelis de Vietro <italolelis@gmail.com>
2016-11-15 18:18:08 +0100

    Fixed typo

--- ci/scripts/docker-deployment.sh
+++ ci/scripts/docker-deployment.sh
@@ -14,4 +14,4 @@ cp -r "source-code/"* docker-images/latest
 # Copies code to the image directories
 cp docker-images/dev/ci/assets/Dockerfile.dev docker-images/dev/Dockerfile
 echo "dev" >> docker-images/dev/version
-cp docker-images/latest/ci/assets/Dockerfile docker-images/dev/Dockerfile 
+cp docker-images/latest/ci/assets/Dockerfile docker-images/latest/Dockerfile
