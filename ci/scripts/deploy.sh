a635a9bf2c94ef6c58d2c7e0243fea7e247de6ab
Italo Lelis de Vietro <italolelis@gmail.com>
2016-11-15 11:06:42 +0100

    FIxed deployment

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,20 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
+# Ensure this script fails if anything errors
 set -e
-base_path="$(pwd)"
-# Requires $GROUP_NAME to be defined
-# Requires $TAR_FILE to be defined
-# Optional $INPUT defaults to release
-# Optional ${DEPLOYMENT_ENVIRONMENT} default to staging.ini
-RC_VERSION=$(cat ${VERSION_FILE})
-INPUT=${INPUT-release}
-CI_INVENTORY=${DEPLOYMENT_ENVIRONMENT-staging.ini}
+
+# Copy the private key for access to deploy
 mkdir -p /root/.ssh/ && chmod 0600 /root/.ssh
-set +x
-echo "${PRIVATE_KEY}" > "${ANSIBLE_PRIVATE_KEY_FILE}" && chmod 0600 "${ANSIBLE_PRIVATE_KEY_FILE}"
-set -x
-# Check we have right params
-[ ! -f "${base_path}/${TAR_FILE}" ] && echo "tar file not found '${base_path}/${TAR_FILE}'" && exit 1
-zcat ${TAR_FILE} | tar -xf -
-cd "automation-artifcat/plays"
+echo "${DEPLOYMENT_PRIVATE_KEY}" > /root/.ssh/id_rsa && chmod 0600 /root/.ssh/id_rsa
+
+# Untar the automation release
+zcat automation-source-code/automation-artifcat.tar.gz | tar -xf -
+
+# Change to plays directory
+cd automation-artifcat/plays
+
+# Temporary fix for the socket
+sed -i 's/%%h-%%p-%%r/%%h-%%r/g' ansible.cfg
+
+# Deploy to staging using ansible
 export ANSIBLE_FORCE_COLOR=true
-ansible-playbook -i ../${DEPLOYMENT_ENVIRONMENT} ${GROUP_NAME}.yml -u policy -vvvv -t deployment -e deployment_force=true
+ansible-playbook \
+    -i ../${DEPLOYMENT_ENVIRONMENT}.ini \
+    -u policy \
+    -vvvv \
+    -t deployment \
+    -e deployment_force=true \
+    ${GROUP_NAME}.yml
