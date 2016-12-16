ecee12c681ad99ac569d4883c98ecb5ef8473053
Kieran <me@kieranajp.co.uk>
2016-12-16 16:08:46 +0100

    Update Oauth2SecretMiddleware to remove gin

--- oauth2_secret_middleware.go
+++ oauth2_secret_middleware.go
@@ -8,39 +8,41 @@ import (
 	"encoding/base64"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
 	"github.com/hellofresh/janus/errors"
 )
 
-type Oauth2Secret struct {
+// Oauth2SecretMiddleware prevents requests to an API from exceeding a specified rate limit.
+type Oauth2SecretMiddleware struct {
 	oauthSpec *OAuthSpec
 }
 
-func (m *Oauth2Secret) ProcessRequest(req *http.Request, c *gin.Context) (error, int) {
+// ProcessRequest is the middleware method.
+func (m *Oauth2SecretMiddleware) ProcessRequest(rw http.ResponseWriter, req *http.Request) (int, error) {
 	log.Debug("Starting Oauth2Secret middleware")
 
 	if "" != req.Header.Get("Authorization") {
 		log.Debug("Authorization is set, proxying")
-		return nil, http.StatusOK
+		return http.StatusOK, nil
 	}
 
 	clientID := req.URL.Query().Get("client_id")
 	if "" == clientID {
 		log.Debug("ClientID not set, proxying")
-		return nil, http.StatusOK
+		return http.StatusOK, nil
 	}
 
 	clientSecret, exists := m.oauthSpec.Secrets[clientID]
 	if false == exists {
 		err := errors.ErrClientIdNotFound
-		return err, err.Code
+		return err.Code, err
 	}
 
 	m.ChangeRequest(req, clientID, clientSecret)
-	return nil, http.StatusOK
+	return http.StatusOK, nil
 }
 
-func (m *Oauth2Secret) ChangeRequest(req *http.Request, clientID, clientSecret string) {
+// ChangeRequest modifies the request to add the Authorization headers.
+func (m *Oauth2SecretMiddleware) ChangeRequest(req *http.Request, clientID, clientSecret string) {
 	log.Debug("Modifying request")
 	authString := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", clientID, clientSecret)))
 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", authString))
