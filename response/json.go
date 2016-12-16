2bb73997547fb14fc0b46e93d52b1a9df3d90e14
Italo Lelis de Vietro <italolelis@gmail.com>
2016-12-16 16:56:00 +0100

    Added response json encapsulation

---
+++ response/json.go
@@ -0,0 +1,14 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+func JSON(w http.ResponseWriter, code int, obj interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(obj)
+}
