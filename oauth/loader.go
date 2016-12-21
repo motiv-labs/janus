84f6fc296a67ce2afa173c1c63de5868faa3e7ba
Italo Lelis de Vietro <italolelis@gmail.com>
2016-12-21 10:49:04 +0100

    Loaders now uses a channel to register routes

--- oauth/loader.go
+++ oauth/loader.go
@@ -1,45 +1,48 @@
 package oauth
 
 import (
-	log "github.com/Sirupsen/logrus"
 	"github.com/hellofresh/janus/cors"
+	"github.com/hellofresh/janus/log"
 	"github.com/hellofresh/janus/middleware"
 	"github.com/hellofresh/janus/proxy"
-	"github.com/hellofresh/janus/router"
 )
 
 // Loader handles the loading of the api specs
 type Loader struct {
-	proxyRegister *proxy.Register
-	accessor      *middleware.DatabaseAccessor
-	debug         bool
+	registerChan *proxy.RegisterChan
+	accessor     *middleware.DatabaseAccessor
+	debug        bool
 }
 
 // NewLoader creates a new instance of the api manager
-func NewLoader(router router.Router, accessor *middleware.DatabaseAccessor, proxyRegister *proxy.Register, debug bool) *Loader {
-	return &Loader{proxyRegister, accessor, debug}
+func NewLoader(registerChan *proxy.RegisterChan, accessor *middleware.DatabaseAccessor, debug bool) *Loader {
+	return &Loader{registerChan, accessor, debug}
 }
 
 // Load loads all api specs from a datasource
 func (m *Loader) Load() {
 	oAuthServers := m.getOAuthServers()
-	go m.LoadOAuthServers(oAuthServers)
+	m.RegisterOAuthServers(oAuthServers)
 }
 
-// LoadOAuthServers loads and register the oauth servers
-func (m *Loader) LoadOAuthServers(oauthServers []*OAuth) {
+// RegisterOAuthServers register many oauth servers
+func (m *Loader) RegisterOAuthServers(oauthServers []*OAuth) {
 	log.Debug("Loading OAuth servers configurations")
 
 	for _, oauthServer := range oauthServers {
-		proxies := GetProxiesForServer(oauthServer)
-		m.proxyRegister.RegisterMany(
-			proxies,
-			NewSecretMiddleware(oauthServer).Handler,
-			cors.NewMiddleware(oauthServer.CorsMeta, m.debug).Handler,
-		)
+		m.registerChan.Many <- m.RegisterOAuthServer(oauthServer)
 	}
 
 	log.Debug("Done loading OAuth servers configurations")
+}
+
+// RegisterOAuthServer register the an oauth server
+func (m *Loader) RegisterOAuthServer(oauthServer *OAuth) []*proxy.Route {
+	return GetRoutesForServer(
+		oauthServer,
+		NewSecretMiddleware(oauthServer).Handler,
+		cors.NewMiddleware(oauthServer.CorsMeta, m.debug).Handler,
+	)
 }
 
 //getOAuthServers Load oauth servers from datasource
