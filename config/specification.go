1393c3199b5c9419ebb38e2189baaad084f46a9e
Italo Lelis de Vietro <italolelis@gmail.com>
2016-12-21 10:47:23 +0100

    Added some extra configuration

--- config/specification.go
+++ config/specification.go
@@ -4,14 +4,23 @@ import "github.com/kelseyhightower/envconfig"
 
 // Specification for basic configurations
 type Specification struct {
-	DatabaseDSN  string `envconfig:"DATABASE_DSN"`
-	Port         int    `envconfig:"PORT"`
-	Debug        bool   `envconfig:"DEBUG"`
-	StatsdDSN    string `envconfig:"STATSD_DSN"`
-	StatsdPrefix string `envconfig:"STATSD_PREFIX"`
-	StorageDSN   string `envconfig:"REDIS_DSN"`
-	Credentials  Credentials
-	Application  Application
+	DatabaseDSN         string `envconfig:"DATABASE_DSN"`
+	Port                int    `envconfig:"PORT"`
+	Debug               bool   `envconfig:"DEBUG"`
+	LogLevel            string `envconfig:"LOG_LEVEL" default:"info"`
+	GraceTimeOut        int64  `envconfig:"GRACE_TIMEOUT"`
+	MaxIdleConnsPerHost int    `description:"Disable SSL certificate verification" envconfig:"MAX_IDLE_CONNS_PER_HOST"`
+	InsecureSkipVerify  bool   `description:"Disable SSL certificate verification"`
+	StorageDSN          string `envconfig:"REDIS_DSN"`
+	Statsd              Statsd
+	Credentials         Credentials
+	Application         Application
+}
+
+// Statsd holds the configuration for statsd
+type Statsd struct {
+	DSN    string `envconfig:"STATSD_DSN"`
+	Prefix string `envconfig:"STATSD_PREFIX"`
 }
 
 type Application struct {
@@ -20,9 +29,9 @@ type Application struct {
 }
 
 type Credentials struct {
-	Secret   string `envconfig:"SECRET"`
-	Username string `envconfig:"ADMIN_USERNAME"`
-	Password string `envconfig:"ADMIN_PASSWORD"`
+	Secret   string `envconfig:"SECRET" required:"true"`
+	Username string `envconfig:"ADMIN_USERNAME" default:"admin"`
+	Password string `envconfig:"ADMIN_PASSWORD" default:"admin"`
 }
 
 //LoadEnv loads environment variables
