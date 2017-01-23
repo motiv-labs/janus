832293d547c036c5205b85761574e997067526db
Italo Lelis de Vietro <italolelis@gmail.com>
2017-01-23 15:44:23 +0100

    Created in momory storage

---
+++ pkg/store/in_memory.go
@@ -0,0 +1,65 @@
+package store
+
+import (
+	"sync"
+	"time"
+
+	"github.com/ulule/limiter"
+)
+
+// InMemoryStore is the redis store.
+type InMemoryStore struct {
+	sync.Mutex
+	data map[string]string
+}
+
+// NewInMemoryStore returns an instance of memory store.
+func NewInMemoryStore() *InMemoryStore {
+	return NewInMemoryStoreWithOptions()
+}
+
+// NewInMemoryStoreWithOptions returns an instance of memory store with custom options.
+func NewInMemoryStoreWithOptions() *InMemoryStore {
+	return &InMemoryStore{
+		data: make(map[string]string),
+	}
+}
+
+func (s *InMemoryStore) Exists(key string) (bool, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	return s.exists(key)
+}
+
+func (s *InMemoryStore) Get(key string) (string, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	return s.get(key)
+}
+
+func (s *InMemoryStore) Set(key string, value string, expire time.Duration) error {
+	s.Lock()
+	defer s.Unlock()
+
+	return s.set(key, value, expire)
+}
+
+func (s *InMemoryStore) ToLimiterStore(prefix string) (limiter.Store, error) {
+	return limiter.NewMemoryStore(), nil
+}
+
+func (s *InMemoryStore) exists(key string) (bool, error) {
+	_, exists := s.data[key]
+	return exists, nil
+}
+
+func (s *InMemoryStore) get(key string) (string, error) {
+	return s.data[key], nil
+}
+
+func (s *InMemoryStore) set(key string, value string, expire time.Duration) error {
+	s.data[key] = value
+	return nil
+}
