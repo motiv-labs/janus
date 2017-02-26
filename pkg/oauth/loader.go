420bf2463488b7c27bc372d5f1e73dedacb6e007
Italo Lelis de Vietro <italolelis@gmail.com>
2017-02-26 16:51:33 +0100

    Removed register and moved to loader

--- pkg/oauth/loader.go
+++ pkg/oauth/loader.go
@@ -5,18 +5,20 @@ import (
 	"github.com/hellofresh/janus/pkg/cors"
 	"github.com/hellofresh/janus/pkg/middleware"
 	"github.com/hellofresh/janus/pkg/proxy"
+	"github.com/hellofresh/janus/pkg/store"
 )
 
 // Loader handles the loading of the api specs
 type Loader struct {
 	register *proxy.Register
+	store    store.Store
 	accessor *middleware.DatabaseAccessor
 	debug    bool
 }
 
 // NewLoader creates a new instance of the api manager
-func NewLoader(register *proxy.Register, accessor *middleware.DatabaseAccessor, debug bool) *Loader {
-	return &Loader{register, accessor, debug}
+func NewLoader(register *proxy.Register, store store.Store, accessor *middleware.DatabaseAccessor, debug bool) *Loader {
+	return &Loader{register, store, accessor, debug}
 }
 
 // Load loads all api specs from a datasource
@@ -30,19 +32,59 @@ func (m *Loader) RegisterOAuthServers(oauthServers []*OAuth) {
 	log.Debug("Loading OAuth servers configurations")
 
 	for _, oauthServer := range oauthServers {
-		m.register.AddMany(m.RegisterOAuthServer(oauthServer))
+		log.Debug("Loading oauth configuration")
+		corsHandler := cors.NewMiddleware(oauthServer.CorsMeta, m.debug).Handler
+		//oauth proxy
+		log.Debug("Registering authorize endpoint")
+		authorizeProxy := oauthServer.Endpoints.Authorize
+		if proxy.Validate(authorizeProxy) {
+			m.register.Add(proxy.NewRoute(authorizeProxy, corsHandler))
+		} else {
+			log.Debug("No authorize endpoint")
+		}
+
+		log.Debug("Registering token endpoint")
+		tokenProxy := oauthServer.Endpoints.Token
+		if proxy.Validate(tokenProxy) {
+			m.register.Add(proxy.NewRoute(tokenProxy, NewSecretMiddleware(oauthServer).Handler, corsHandler))
+		} else {
+			log.Debug("No token endpoint")
+		}
+
+		log.Debug("Registering info endpoint")
+		infoProxy := oauthServer.Endpoints.Info
+		if proxy.Validate(infoProxy) {
+			m.register.Add(proxy.NewRoute(infoProxy, corsHandler))
+		} else {
+			log.Debug("No info endpoint")
+		}
+
+		log.Debug("Registering revoke endpoint")
+		revokeProxy := oauthServer.Endpoints.Revoke
+		if proxy.Validate(revokeProxy) {
+			m.register.Add(proxy.NewRoute(revokeProxy, corsHandler, NewRevokeMiddleware(m.store).Handler))
+		} else {
+			log.Debug("No revoke endpoint")
+		}
+
+		log.Debug("Registering create client endpoint")
+		createProxy := oauthServer.ClientEndpoints.Create
+		if proxy.Validate(createProxy) {
+			m.register.Add(proxy.NewRoute(createProxy, corsHandler))
+		} else {
+			log.Debug("No client create endpoint")
+		}
+
+		log.Debug("Registering remove client endpoint")
+		removeProxy := oauthServer.ClientEndpoints.Remove
+		if proxy.Validate(removeProxy) {
+			m.register.Add(proxy.NewRoute(removeProxy, corsHandler))
+		} else {
+			log.Debug("No client remove endpoint")
+		}
 	}
 
 	log.Debug("Done loading OAuth servers configurations")
-}
-
-// RegisterOAuthServer register the an oauth server
-func (m *Loader) RegisterOAuthServer(oauthServer *OAuth) []*proxy.Route {
-	return GetRoutesForServer(
-		oauthServer,
-		NewSecretMiddleware(oauthServer).Handler,
-		cors.NewMiddleware(oauthServer.CorsMeta, m.debug).Handler,
-	)
 }
 
 //getOAuthServers Load oauth servers from datasource
