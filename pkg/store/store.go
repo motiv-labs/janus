649e15ac1c261171fbfccfd352ae8746eaec2723
Italo Lelis de Vietro <italolelis@gmail.com>
2017-02-28 16:31:52 +0100

    Improved the store interface

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,9 +1,18 @@
 package store
 
 import (
+	"net/url"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/garyburd/redigo/redis"
 	"github.com/ulule/limiter"
+)
+
+const (
+	InMemory = "memory"
+	Redis    = "redis"
+	None     = "none"
 )
 
 // Store is the common interface for datastores.
@@ -25,4 +34,29 @@ type Options struct {
 
 	// CleanUpInterval is the interval for cleanup.
 	CleanUpInterval time.Duration
+}
+
+func Build(dsn string) (Store, error) {
+	url, err := url.Parse(dsn)
+	if nil != err {
+		log.Panic(err)
+	}
+	log.WithField("type", url.Scheme).Debug("Initializing storage")
+
+	switch url.Scheme {
+	case InMemory:
+		return NewInMemoryStore(), nil
+	case Redis:
+		// Create a Redis pool.
+		pool := &redis.Pool{
+			MaxIdle:     3,
+			IdleTimeout: 240 * time.Second,
+			Dial:        func() (redis.Conn, error) { return redis.DialURL(dsn) },
+		}
+
+		log.Debugf("Trying to connect to redis pool: %s", dsn)
+		return NewRedisStore(pool)
+	}
+
+	return nil, ErrUnknownStorage
 }
