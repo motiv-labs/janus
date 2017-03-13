f6b170b808fc863562ec08dde830ad18e97c15f6
Italo Lelis de Vietro <italolelis@gmail.com>
2017-03-13 17:19:50 +0100

    No need to depend on mgo

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hellofresh/janus/pkg/cors"
 	"github.com/hellofresh/janus/pkg/oauth"
 	"github.com/hellofresh/janus/pkg/proxy"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // Spec Holds an api definition and basic options
@@ -17,27 +16,25 @@ type Spec struct {
 
 // Definition Represents an API that you want to proxy
 type Definition struct {
-	ID             bson.ObjectId     `bson:"_id,omitempty" json:"id,omitempty" valid:"required"`
-	CreatedAt      time.Time         `bson:"created_at" json:"created_at" valid:"-"`
-	UpdatedAt      time.Time         `bson:"updated_at" json:"updated_at" valid:"-"`
-	Name           string            `bson:"name" json:"name" valid:"required"`
-	Slug           string            `bson:"slug" json:"slug"`
-	Active         bool              `bson:"active" json:"active"`
-	UseBasicAuth   bool              `bson:"use_basic_auth" json:"use_basic_auth"`
-	Domain         string            `bson:"domain" json:"domain"`
-	Proxy          *proxy.Definition `bson:"proxy" json:"proxy" valid:"required"`
-	AllowedIPs     []string          `mapstructure:"allowed_ips" bson:"allowed_ips" json:"allowed_ips"`
-	UseOauth2      bool              `bson:"use_oauth2" json:"use_oauth2"`
-	OAuthServerID  bson.ObjectId     `bson:"oauth_server_id" json:"oauth_server_id"`
-	RateLimit      RateLimitMeta     `bson:"rate_limit" json:"rate_limit" valid:"required"`
-	CorsMeta       cors.Meta         `bson:"cors_meta" json:"cors_meta" valid:"cors_meta"`
-	UseCompression bool              `bson:"use_compression" json:"use_compression"`
+	CreatedAt       time.Time         `bson:"created_at" json:"created_at" valid:"-"`
+	UpdatedAt       time.Time         `bson:"updated_at" json:"updated_at" valid:"-"`
+	Name            string            `bson:"name" json:"name" valid:"required"`
+	Slug            string            `bson:"slug" json:"slug" valid:"required"`
+	Active          bool              `bson:"active" json:"active"`
+	UseBasicAuth    bool              `bson:"use_basic_auth" json:"use_basic_auth"`
+	Domain          string            `bson:"domain" json:"domain"`
+	Proxy           *proxy.Definition `bson:"proxy" json:"proxy" valid:"required"`
+	AllowedIPs      []string          `mapstructure:"allowed_ips" bson:"allowed_ips" json:"allowed_ips"`
+	UseOauth2       bool              `bson:"use_oauth2" json:"use_oauth2"`
+	OAuthServerSlug string            `bson:"oauth_server_slug" json:"oauth_server_slug"`
+	RateLimit       RateLimitMeta     `bson:"rate_limit" json:"rate_limit" valid:"required"`
+	CorsMeta        cors.Meta         `bson:"cors_meta" json:"cors_meta" valid:"cors_meta"`
+	UseCompression  bool              `bson:"use_compression" json:"use_compression"`
 }
 
 // NewDefinition creates a new API Definition with default values
 func NewDefinition() *Definition {
 	return &Definition{
-		OAuthServerID:  bson.NewObjectId(),
 		UseCompression: true,
 	}
 }
