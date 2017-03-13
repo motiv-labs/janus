608f62e3b06c01221ccce0b3e65196df2205c332
Italo Lelis de Vietro <italolelis@gmail.com>
2017-03-13 17:26:05 +0100

    Adapeted main to the new changes

--- cmd/janus/main.go
+++ cmd/janus/main.go
@@ -4,82 +4,56 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/NYTimes/gziphandler"
+	mgo "gopkg.in/mgo.v2"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/hellofresh/janus/pkg/api"
-	"github.com/hellofresh/janus/pkg/jwt"
 	"github.com/hellofresh/janus/pkg/middleware"
 	"github.com/hellofresh/janus/pkg/oauth"
 	"github.com/hellofresh/janus/pkg/proxy"
 	"github.com/hellofresh/janus/pkg/router"
 	"github.com/hellofresh/janus/pkg/stats"
+	"github.com/hellofresh/janus/pkg/web"
 )
 
-//loadAPIEndpoints register api endpoints
-func loadAPIEndpoints(router router.Router, authMiddleware *jwt.Middleware) {
-	log.Debug("Loading API Endpoints")
-
-	// Apis endpoints
-	handler := api.NewController()
-	group := router.Group("/apis")
-	group.Use(authMiddleware.Handler, gziphandler.GzipHandler)
-	{
-		group.GET("", handler.Get())
-		group.POST("", handler.Post())
-		group.GET("/:id", handler.GetBy())
-		group.PUT("/:id", handler.PutBy())
-		group.DELETE("/:id", handler.DeleteBy())
-	}
-}
-
-//loadOAuthEndpoints register api endpoints
-func loadOAuthEndpoints(router router.Router, authMiddleware *jwt.Middleware) {
-	log.Debug("Loading OAuth Endpoints")
-
-	// Oauth servers endpoints
-	oAuthHandler := oauth.NewController()
-	oauthGroup := router.Group("/oauth/servers")
-	oauthGroup.Use(authMiddleware.Handler, gziphandler.GzipHandler)
-	{
-		oauthGroup.GET("", oAuthHandler.Get())
-		oauthGroup.POST("", oAuthHandler.Post())
-		oauthGroup.GET("/:id", oAuthHandler.GetBy())
-		oauthGroup.PUT("/:id", oAuthHandler.PutBy())
-		oauthGroup.DELETE("/:id", oAuthHandler.DeleteBy())
-	}
-}
-
-func loadAuthEndpoints(router router.Router, authMiddleware *jwt.Middleware) {
-	log.Debug("Loading Auth Endpoints")
-
-	handlers := jwt.Handler{Config: authMiddleware.Config}
-	router.POST("/login", handlers.Login())
-	authGroup := router.Group("/auth")
-	authGroup.Use(authMiddleware.Handler)
-	{
-		authGroup.GET("/refresh_token", handlers.Refresh())
-	}
-}
-
 func main() {
-	defer accessor.Close()
+	var repo api.APISpecRepository
+	var oAuthServersRepo oauth.Repository
+	var err error
+
 	defer statsdClient.Close()
 
 	statsClient := stats.NewStatsClient(statsdClient)
 
-	// create router
-	r := router.NewHttpTreeMuxRouter()
-	r.Use(
-		middleware.NewStats(statsClient).Handler,
-		middleware.NewLogger(globalConfig.Debug).Handler,
-		middleware.NewRecovery(RecoveryHandler).Handler,
-		middleware.NewMongoDB(accessor).Handler,
-	)
+	if globalConfig.UseDBAppConfigs {
+		log.Debugf("Trying to connect to %s", globalConfig.Database.DSN)
+		session, err := mgo.Dial(globalConfig.Database.DSN)
+		defer session.Close()
+		if err != nil {
+			log.Panic(err)
+		}
 
-	// create the proxy
-	oAuthServersRepo, err := oauth.NewMongoRepository(accessor.Session.DB(""))
-	if err != nil {
-		log.Panic(err)
+		log.Debug("Connected to mongodb")
+		session.SetMode(mgo.Monotonic, true)
+
+		log.Debug("Loading API definitions from Mongo DB")
+		repo, err = api.NewMongoAppRepository(session)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// create the proxy
+		log.Debug("Loading OAuth servers definitions from Mongo DB")
+		oAuthServersRepo, err = oauth.NewMongoRepository(session)
+		if err != nil {
+			log.Panic(err)
+		}
+	} else {
+		log.Debug("Using File base configuration")
+		repo, err = api.NewFileSystemRepository("")
+		if err != nil {
+			log.Panic(err)
+		}
 	}
 
 	transport := oauth.NewAwareTransport(statsClient, storage, oAuthServersRepo)
@@ -92,25 +66,30 @@ func main() {
 	})
 	defer p.Close()
 
+	// create router
+	r := router.NewHttpTreeMuxRouter()
+	r.Use(
+		middleware.NewStats(statsClient).Handler,
+		middleware.NewLogger().Handler,
+		middleware.NewRecovery(web.RecoveryHandler).Handler,
+	)
+
 	// create proxy register
 	register := proxy.NewRegister(r, p)
-	apiLoader := api.NewLoader(register, storage, oAuthServersRepo, accessor, globalConfig.Debug)
-	apiLoader.Load()
 
-	oauthLoader := oauth.NewLoader(register, storage, accessor, globalConfig.Debug)
-	oauthLoader.Load()
+	apiLoader := api.NewLoader(register, storage, oAuthServersRepo)
+	apiLoader.LoadDefinitions(repo)
 
-	// create authentication for Janus
-	authConfig := jwt.NewConfig(globalConfig.Credentials)
-	authMiddleware := jwt.NewMiddleware(authConfig)
+	oauthLoader := oauth.NewLoader(register, storage)
+	oauthLoader.LoadDefinitions(oAuthServersRepo)
 
-	// create endpoints
-	r.GET("/", Home(globalConfig.Application))
-	r.GET("/status", Heartbeat())
-
-	loadAuthEndpoints(r, authMiddleware)
-	loadAPIEndpoints(r, authMiddleware)
-	loadOAuthEndpoints(r, authMiddleware)
+	wp := web.Provider{
+		Port:     "3001",
+		Cred:     globalConfig.Credentials,
+		APIRepo:  repo,
+		AuthRepo: oAuthServersRepo,
+	}
+	wp.Provide()
 
 	log.Fatal(listenAndServe(r))
 }
