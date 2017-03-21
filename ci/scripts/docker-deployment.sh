f6003b5a19b89ec6fc1a2797fbfde6c9fba060b3
Italo Lelis de Vietro <italolelis@gmail.com>
2017-03-21 18:30:45 +0100

    Fixed tar script

--- ci/scripts/docker-deployment.sh
+++ ci/scripts/docker-deployment.sh
@@ -6,9 +6,11 @@ set -e
 # Creates the necessary directories
 mkdir -p docker-images
 mkdir -p docker-images/dist
+mkdir -p docker-images/ci/assets
 
 # unzip the binary
-tar -zxf -C docker-images/dist release-candidate/janus.tar.gz
+tar -C docker-images/dist -zxf release-candidate/janus.tar.gz
 
 # Copies code to the image directories
-cp docker-images/ci/assets/Dockerfile docker-images/Dockerfile
+cp source-code/ci/assets/* docker-images/ci/assets
+cp source-code/Dockerfile docker-images/Dockerfile
