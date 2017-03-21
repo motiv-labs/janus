488a75454c9cf138905541aa0363d37c00ebd4bd
Italo Lelis de Vietro <italolelis@gmail.com>
2017-03-21 15:23:38 +0100

    Changed to bash

--- build/build.sh
+++ build/build.sh
@@ -1,18 +1,4 @@
-#!/bin/sh
-#
-# Copyright 2016 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/bin/bash
 
 set -e
 
@@ -20,8 +6,8 @@ set -e
 rm -f dist/janus_*
 
 # Build 386 amd64 binaries
-OS_PLATFORM_ARG="linux darwin windows freebsd openbsd"
-OS_ARCH_ARG="386 amd64"
+OS_PLATFORM_ARG=(linux darwin windows freebsd openbsd)
+OS_ARCH_ARG=(386 amd64)
 for OS in ${OS_PLATFORM_ARG[@]}; do
   for ARCH in ${OS_ARCH_ARG[@]}; do
     echo "Building binary for $OS/$ARCH..."
@@ -30,8 +16,8 @@ for OS in ${OS_PLATFORM_ARG[@]}; do
 done
 
 # Build arm binaries
-OS_PLATFORM_ARG="linux"
-OS_ARCH_ARG="arm arm64"
+OS_PLATFORM_ARG=(linux)
+OS_ARCH_ARG=(arm arm64)
 for OS in ${OS_PLATFORM_ARG[@]}; do
   for ARCH in ${OS_ARCH_ARG[@]}; do
     echo "Building binary for $OS/$ARCH..."
