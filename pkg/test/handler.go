12e68e6400debb85f1b0401764cef7863b0e29d9
Italo Lelis de Vietro <italolelis@gmail.com>
2017-03-30 17:16:54 +0200

    Added missing comment

--- pkg/test/handler.go
+++ pkg/test/handler.go
@@ -2,6 +2,7 @@ package test
 
 import "net/http"
 
+// Ping is a test handler
 func Ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.Write([]byte("OK\n"))
