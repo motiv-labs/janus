bb4d41fec70cfd91b475a8be472de297d012d099
Italo Lelis de Vietro <italolelis@gmail.com>
2017-04-22 02:06:55 +0200

    Changed server to comply with notifier

--- cmd/janus/server.go
+++ cmd/janus/server.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/garyburd/redigo/redis"
 	"github.com/hellofresh/janus/pkg/api"
 	"github.com/hellofresh/janus/pkg/config"
 	"github.com/hellofresh/janus/pkg/errors"
 	"github.com/hellofresh/janus/pkg/loader"
 	"github.com/hellofresh/janus/pkg/middleware"
+	"github.com/hellofresh/janus/pkg/notifier"
 	"github.com/hellofresh/janus/pkg/oauth"
 	"github.com/hellofresh/janus/pkg/plugin"
 	"github.com/hellofresh/janus/pkg/proxy"
 	"github.com/hellofresh/janus/pkg/router"
 	"github.com/hellofresh/janus/pkg/store"
 	"github.com/hellofresh/janus/pkg/web"
+	stats "github.com/hellofresh/stats-go"
 	"github.com/spf13/cobra"
 	mgo "gopkg.in/mgo.v2"
+)
+
+const (
+	PubSubChannel = "janus.cluster.notifications"
 )
 
 var (
@@ -26,6 +35,11 @@ var (
 	oAuthServersRepo oauth.Repository
 	err              error
 	globalConfig     *config.Specification
+	statsClient      stats.Client
+	storage          store.Store
+	server           *http.Server
+	prx              *proxy.Proxy
+	pluginLoader     *plugin.Loader
 )
 
 // RunServer is the run command to start Janus
@@ -39,9 +53,11 @@ func RunServer(cmd *cobra.Command, args []string) {
 
 	initLogger(globalConfig.LogLevel)
 	initTracing(globalConfig.Tracing)
-	storage := initStorage(globalConfig.Storage)
-	statsClient := initStatsdClient(globalConfig.Stats)
+	storage = initStorage(globalConfig.Storage)
+	statsClient = initStatsdClient(globalConfig.Stats)
 	defer statsClient.Close()
+
+	go startPubSubLoop(storage)
 
 	dsnURL, err := url.Parse(globalConfig.Database.DSN)
 	switch dsnURL.Scheme {
@@ -88,50 +104,22 @@ func RunServer(cmd *cobra.Command, args []string) {
 		log.WithError(errors.ErrInvalidScheme).Error("No Database selected")
 	}
 
-	p := proxy.WithParams(proxy.Params{
+	prx = proxy.WithParams(proxy.Params{
 		StatsClient:            statsClient,
 		FlushInterval:          globalConfig.BackendFlushInterval,
 		IdleConnectionsPerHost: globalConfig.MaxIdleConnsPerHost,
 		CloseIdleConnsPeriod:   globalConfig.CloseIdleConnsPeriod,
 		InsecureSkipVerify:     globalConfig.InsecureSkipVerify,
 	})
-	defer p.Close()
+	defer prx.Close()
 
-	// create router with a custom not found handler
-	router.DefaultOptions.NotFoundHandler = web.NotFound
-	r := router.NewChiRouterWithOptions(router.DefaultOptions)
-	r.Use(
-		middleware.NewStats(statsClient).Handler,
-		middleware.NewLogger().Handler,
-		middleware.NewRecovery(web.RecoveryHandler).Handler,
-		middleware.NewOpenTracing(globalConfig.Web.IsHTTPS()).Handler,
-	)
-
-	pluginLoader := plugin.NewLoader()
+	pluginLoader = plugin.NewLoader()
 	pluginLoader.Add(
 		plugin.NewRateLimit(storage),
 		plugin.NewCORS(),
 		plugin.NewOAuth2(oAuthServersRepo, storage),
 		plugin.NewCompression(),
 	)
-
-	// create proxy register
-	register := proxy.NewRegister(r, p)
-	var (
-		apiSubs   *store.Subscription
-		oauthSubs *store.Subscription
-	)
-
-	if subscriber, ok := storage.(store.Subscriber); ok {
-		apiSubs = subscriber.Subscribe("api_updates")
-		oauthSubs = subscriber.Subscribe("oauth_updates")
-	}
-
-	apiLoader := loader.NewAPILoader(register, pluginLoader, apiSubs)
-	apiLoader.LoadDefinitions(repo)
-
-	oauthLoader := loader.NewOAuthLoader(register, storage, oauthSubs)
-	oauthLoader.LoadDefinitions(oAuthServersRepo)
 
 	wp := web.Provider{
 		Port:     globalConfig.Web.Port,
@@ -144,22 +132,93 @@ func RunServer(cmd *cobra.Command, args []string) {
 	}
 
 	if publisher, ok := storage.(store.Publisher); ok {
-		wp.Publisher = publisher
+		wp.Notifier = notifier.New(publisher, PubSubChannel)
 	}
 
 	wp.Provide(version)
 
-	log.Fatal(listenAndServe(r))
-}
-
-func listenAndServe(handler http.Handler) error {
+	r := load()
 	address := fmt.Sprintf(":%v", globalConfig.Port)
 	log.WithField("address", address).Info("Listening on")
+	server = &http.Server{Addr: address, Handler: r}
+	log.Fatal(listenAndServe(server))
+}
+
+func listenAndServe(server *http.Server) error {
 	log.Info("Janus started")
 	if globalConfig.Web.IsHTTPS() {
-		return http.ListenAndServeTLS(address, globalConfig.CertFile, globalConfig.KeyFile, handler)
+		return server.ListenAndServeTLS(globalConfig.CertFile, globalConfig.KeyFile)
 	}
 
 	log.Info("Certificate and certificate key were not found, defaulting to HTTP")
-	return http.ListenAndServe(address, handler)
+	return server.ListenAndServe()
+}
+
+func load() router.Router {
+	r := createRouter()
+
+	// create proxy register
+	register := proxy.NewRegister(r, prx)
+
+	apiLoader := loader.NewAPILoader(register, pluginLoader)
+	apiLoader.LoadDefinitions(repo)
+
+	oauthLoader := loader.NewOAuthLoader(register, storage)
+	oauthLoader.LoadDefinitions(oAuthServersRepo)
+
+	return r
+}
+
+func createRouter() router.Router {
+	// create router with a custom not found handler
+	router.DefaultOptions.NotFoundHandler = web.NotFound
+	r := router.NewChiRouterWithOptions(router.DefaultOptions)
+	r.Use(
+		middleware.NewStats(statsClient).Handler,
+		middleware.NewLogger().Handler,
+		middleware.NewRecovery(web.RecoveryHandler).Handler,
+		middleware.NewOpenTracing(globalConfig.Web.IsHTTPS()).Handler,
+	)
+	return r
+}
+
+func startPubSubLoop(storage store.Store) {
+	log.Debug("Listening for changes")
+
+	if subscriber, ok := storage.(store.Subscriber); ok {
+		for {
+			err := subscriber.Subscribe(PubSubChannel, func(v interface{}) {
+				handleEvent(v)
+			})
+			if err != nil {
+				log.WithFields(log.Fields{
+					"prefix": "pub-sub",
+					"err":    err,
+				}).Error("Connection failed, reconnect in 10s")
+
+				time.Sleep(10 * time.Second)
+				log.WithFields(log.Fields{
+					"prefix": "pub-sub",
+				}).Warning("Reconnecting")
+			}
+		}
+	}
+}
+
+func handleEvent(v interface{}) {
+	message, ok := v.(redis.Message)
+	if !ok {
+		return
+	}
+
+	notif := notifier.Notification{}
+	if err := json.Unmarshal(message.Data, &notif); err != nil {
+		log.Error("Unmarshalling message body failed, malformed: ", err)
+		return
+	}
+
+	if notifier.RequireReload(notif.Command) {
+		newRouter := load()
+		server.Handler = newRouter
+	}
 }
