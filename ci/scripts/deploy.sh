a3848db3bdc0897972fbcdcc1ed5ccf048e82d35
Vladimir Garvardt <vgarvardt@gmail.com>
2017-04-24 10:43:24 +0200

    PLAT-333 Fixed ci to work with automation source code

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -8,12 +8,11 @@ mkdir -p /root/.ssh/ && chmod 0600 /root/.ssh
 echo "${DEPLOYMENT_PRIVATE_KEY}" > /root/.ssh/id_rsa && chmod 0600 /root/.ssh/id_rsa
 
 # Untar the automation release
-echo "Using the following automation artifact:"
-ls -1 automation-source-code/
-tar -xf automation-source-code/janus-automation-*.tar.gz
+echo "Using the following automation artifact version: `cat automation-source-code/version`"
+tar -xf automation-source-code/source.tar.gz
 
 # Change to plays directory
-cd janus-automation-*/plays
+cd hellofresh-janus-automation-*/plays
 
 # Deploy to staging using ansible
 export ANSIBLE_FORCE_COLOR=true
