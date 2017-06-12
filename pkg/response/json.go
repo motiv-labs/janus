4b978b9d037ae06824f404ad350d3e9c38f8d25f
Italo Lelis de Vietro <italolelis@gmail.com>
2017-06-12 19:04:38 +0200

    Fixed bug on delete operation

--- pkg/response/json.go
+++ pkg/response/json.go
@@ -13,7 +13,7 @@ func JSON(w http.ResponseWriter, code int, obj interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 
-	if nil != obj || http.StatusNoContent == code {
+	if nil != obj && http.StatusNoContent != code {
 		err := json.NewEncoder(w).Encode(obj)
 		if err != nil {
 			panic(err.Error())
