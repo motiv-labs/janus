8a5516804ecea8ca3766347a3c0ea10c28417e38
Italo Lelis de Vietro <italolelis@gmail.com>
2017-08-30 12:07:41 +0200

    Refactored the jwt handlers

--- pkg/jwt/middleware.go
+++ pkg/jwt/middleware.go
@@ -1,18 +1,17 @@
 package jwt
 
 import (
-	"context"
-	"errors"
 	"net/http"
-
-	jwt "github.com/dgrijalva/jwt-go"
 )
 
 // Payload Represents the context key
 type Payload struct{}
 
-// UserID Represents the user context key
-type UserID struct{}
+// User represents a logged in user
+type User struct {
+	Username string
+	Email    string
+}
 
 // Middleware struct contains data and logic required for middleware functionality
 type Middleware struct {
@@ -28,21 +27,9 @@ func NewMiddleware(config Guard) *Middleware {
 func (m *Middleware) Handler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		parser := Parser{m.Guard.ParserConfig}
-		token, err := parser.ParseFromRequest(r)
-
+		_, err := parser.ParseFromRequest(r)
 		if err != nil {
 			m.Guard.Unauthorized(w, r, err)
-			return
-		}
-
-		claims := token.Claims.(jwt.MapClaims)
-
-		id := claims["id"].(string)
-		context.WithValue(r.Context(), Payload{}, claims)
-		context.WithValue(r.Context(), UserID{}, id)
-
-		if !m.Guard.Authorizator(id, w, r) {
-			m.Guard.Unauthorized(w, r, errors.New("you don't have permission to access"))
 			return
 		}
 
--- pkg/jwt/token_test.go
+++ pkg/jwt/token_test.go
@@ -13,7 +13,7 @@ func TestIssueAdminToken(t *testing.T) {
 	key := time.Now().Format(time.RFC3339Nano)
 	claimsID := time.Now().Format(time.RFC3339Nano)
 
-	tokenString, err := IssueAdminToken(SigningMethod{alg, key}, claimsID, time.Hour)
+	tokenString, err := IssueAdminToken(SigningMethod{alg, key}, map[string]interface{}{"id": claimsID}, time.Hour)
 	require.NoError(t, err)
 
 	config := NewParserConfig(SigningMethod{Alg: alg, Key: key})
