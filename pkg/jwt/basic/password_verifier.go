20f8cb1b22e2a4c78213ee26814c84c883cb4f15
Italo Lelis de Vietro <italolelis@gmail.com>
2017-08-31 11:17:46 +0200

    Added constant

--- pkg/jwt/basic/password_verifier.go
+++ pkg/jwt/basic/password_verifier.go
@@ -8,24 +8,28 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// User represents a user that wants to login
-type User struct {
+const (
+	contentTypeJSON string = "application/json"
+)
+
+// user represents a user that wants to login
+type user struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 // Equals compares one user to another
-func (u *User) Equals(user *User) bool {
+func (u *user) Equals(user *user) bool {
 	return user.Username == u.Username && user.Password == u.Password
 }
 
 // PasswordVerifier checks if the current user `matches any of the given passwords
 type PasswordVerifier struct {
-	users []*User
+	users []*user
 }
 
 // NewPasswordVerifier creates a new instance of PasswordVerifier
-func NewPasswordVerifier(users []*User) *PasswordVerifier {
+func NewPasswordVerifier(users []*user) *PasswordVerifier {
 	return &PasswordVerifier{users}
 }
 
@@ -51,32 +55,32 @@ func (v *PasswordVerifier) Verify(r *http.Request) (bool, error) {
 	return false, nil
 }
 
-func (v *PasswordVerifier) getUserFromRequest(r *http.Request) (*User, error) {
-	var user *User
+func (v *PasswordVerifier) getUserFromRequest(r *http.Request) (*user, error) {
+	var u *user
 
 	//checks basic auth
 	username, password, ok := r.BasicAuth()
 	if ok {
-		user = &User{
+		u = &user{
 			Username: username,
 			Password: password,
 		}
 	}
 
 	// checks if the content is json otherwise just get from the form params
-	if r.Header.Get("Content-Type") == "application/json" {
-		err := json.NewDecoder(r.Body).Decode(&user)
+	if r.Header.Get("Content-Type") == contentTypeJSON {
+		err := json.NewDecoder(r.Body).Decode(&u)
 		if err != nil {
-			return user, errors.Wrap(err, "could not parse the json body")
+			return u, errors.Wrap(err, "could not parse the json body")
 		}
 	} else {
 		r.ParseForm()
 
-		user = &User{
+		u = &user{
 			Username: r.Form.Get("username"),
 			Password: r.Form.Get("password"),
 		}
 	}
 
-	return user, nil
+	return u, nil
 }
