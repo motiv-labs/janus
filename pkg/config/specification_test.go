ea516c224cb4125c4f5850808ac2f73b8aa72a90
Italo Lelis de Vietro <italolelis@gmail.com>
2017-09-06 20:27:53 +0200

    Added more tests

--- pkg/config/specification_test.go
+++ pkg/config/specification_test.go
@@ -11,6 +11,9 @@ import (
 func Test_LoadEnv(t *testing.T) {
 	os.Setenv("PORT", "8001")
 	os.Setenv("STATS_AUTO_DISCOVER_WHITE_LIST", "api,foo,bar")
+	os.Setenv("BASIC_USERS", "admin:admin, test:test")
+	os.Setenv("GITHUB_ORGANIZATIONS", "hellofresh, tests")
+	os.Setenv("GITHUB_TEAMS", "hellofresh:tests, hellofresh:devs")
 
 	globalConfig, err := LoadEnv()
 	require.NoError(t, err)
@@ -24,4 +27,11 @@ func Test_LoadEnv(t *testing.T) {
 	assert.False(t, globalConfig.TLS.IsHTTPS())
 	assert.False(t, globalConfig.Tracing.IsGoogleCloudEnabled())
 	assert.False(t, globalConfig.Tracing.IsAppdashEnabled())
+	assert.IsType(t, map[string]string{}, globalConfig.Web.Credentials.Basic.Users)
+	assert.Len(t, globalConfig.Web.Credentials.Basic.Users, 2)
+	assert.IsType(t, map[string]string{}, globalConfig.Web.Credentials.Github.Teams)
+	assert.Len(t, globalConfig.Web.Credentials.Github.Teams, 2)
+	assert.IsType(t, []string{}, globalConfig.Web.Credentials.Github.Organizations)
+	assert.Len(t, globalConfig.Web.Credentials.Github.Organizations, 2)
+	assert.True(t, globalConfig.Web.Credentials.Github.IsConfigured())
 }
