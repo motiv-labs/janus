1386f4a91ae5dd4b2fb7278a898d542bdd9e6306
Kobi Meirson <kobi@kidbox.com>
2017-10-14 14:37:47 +0300

    Deprecating garubyrd/redigo in favor of go-redis/redis (as the latter is required for ulule/limiter), as there's no need for 2 redis libraries in the same project. Resolves /hellofresh/janus#220

--- pkg/store/redis.go
+++ pkg/store/redis.go
@@ -2,8 +2,9 @@ package store
 
 import (
 	"encoding/json"
+	"time"
 
-	"github.com/garyburd/redigo/redis"
+	"github.com/go-redis/redis"
 	"github.com/hellofresh/janus/pkg/notifier"
 	log "github.com/sirupsen/logrus"
 )
@@ -15,154 +16,82 @@ type RedisStore struct {
 	// The prefix to use for the key.
 	Prefix string
 
-	// github.com/garyburd/redigo Pool instance.
-	Pool *redis.Pool
+	// go-redis Client instance
+	Client *redis.Client
 
 	// The maximum number of retry under race conditions.
 	MaxRetry int
 }
 
 // NewRedisStore returns an instance of redis store.
-func NewRedisStore(pool *redis.Pool, prefix string) (Store, error) {
+func NewRedisStore(client *redis.Client, prefix string) (Store, error) {
 	if prefix == "" {
 		prefix = defaultPrefix
 	}
 
-	return NewRedisStoreWithOptions(pool, Options{
+	return NewRedisStoreWithOptions(client, Options{
 		Prefix: prefix,
 	})
 }
 
 // NewRedisStoreWithOptions returns an instance of redis store with custom options.
-func NewRedisStoreWithOptions(pool *redis.Pool, options Options) (Store, error) {
+func NewRedisStoreWithOptions(client *redis.Client, options Options) (Store, error) {
 	store := &RedisStore{
-		Pool:   pool,
+		Client: client,
 		Prefix: options.Prefix,
 	}
 
 	return store, nil
 }
 
-func (s *RedisStore) getConnection() redis.Conn {
-	return s.Pool.Get()
-}
-
 // ping checks if redis is alive.
 func (s *RedisStore) ping() (bool, error) {
-	conn := s.getConnection()
-	defer conn.Close()
-
-	data, err := conn.Do("PING")
-	if err != nil || data == nil {
-		return false, err
-	}
-
-	return data == "PONG", nil
+	data, err := s.Client.Ping().Result()
+	return data == "PONG", err
 }
 
 // Exists checks if a key exists in the store
 func (s *RedisStore) Exists(key string) (bool, error) {
-	conn := s.getConnection()
-	defer conn.Close()
-
-	return s.exists(conn, key)
+	data, err := s.Client.Exists(key).Result()
+	return data == 1, err
 }
 
 // Get retreives a value from the store
 func (s *RedisStore) Get(key string) (string, error) {
-	conn := s.getConnection()
-	defer conn.Close()
-
-	return s.get(conn, key)
+	return s.Client.Get(key).Result()
 }
 
 // Remove a value from the store
 func (s *RedisStore) Remove(key string) error {
-	conn := s.getConnection()
-	defer conn.Close()
-
-	return s.remove(conn, key)
+	return s.Client.Del(key).Err()
 }
 
 // Set a value in the store
 func (s *RedisStore) Set(key string, value string, expire int64) error {
-	conn := s.getConnection()
-	defer conn.Close()
-
-	return s.set(conn, key, value, expire)
+	return s.Client.Set(key, value, time.Duration(expire)*time.Second).Err()
 }
 
 // Publish publishes to a topic in redis
 func (s *RedisStore) Publish(topic string, data []byte) error {
-	c := s.getConnection()
-	_, err := c.Do("PUBLISH", topic, data)
-	return err
+	return s.Client.Publish(topic, data).Err()
 }
 
 // Subscribe subscribes to a topic in redis
 func (s *RedisStore) Subscribe(channel string, callback func(notifier.Notification)) error {
-	// Get a connection from a pool
-	c := s.getConnection()
-	defer c.Close()
-
-	psc := redis.PubSubConn{Conn: c}
-	if err := psc.Subscribe(channel); err != nil {
-		return err
-	}
+	pubsub := s.Client.Subscribe(channel)
+	defer pubsub.Close()
 
 	for {
-		switch v := psc.Receive().(type) {
-		case redis.Message:
-			notification := notifier.Notification{}
-			if err := json.Unmarshal(v.Data, &notification); err != nil {
-				log.WithError(err).Error("Unmarshalling message body failed, malformed")
-				return err
-			}
-			callback(notification)
-		case error:
-			log.WithError(v).Debug("An error occurred when getting the message")
-			return v
+		v, err := pubsub.ReceiveMessage()
+		if err != nil {
+			log.WithError(err).Debug("An error occurred when getting the message")
+			return err
 		}
+		notification := notifier.Notification{}
+		if marshallErr := json.Unmarshal([]byte(v.Payload), &notification); marshallErr != nil {
+			log.WithError(marshallErr).Error("Unmarshalling message body failed, malformed")
+			return marshallErr
+		}
+		callback(notification)
 	}
-}
-
-func (s *RedisStore) exists(conn redis.Conn, key string) (bool, error) {
-	exists, err := redis.Bool(conn.Do("EXISTS", key))
-	if err != nil {
-		return false, err
-	}
-
-	return exists, nil
-}
-
-func (s *RedisStore) remove(conn redis.Conn, key string) error {
-	_, err := conn.Do("DEL", key)
-	return err
-}
-
-func (s *RedisStore) get(conn redis.Conn, key string) (string, error) {
-	return redis.String(conn.Do("GET", key))
-}
-
-func (s *RedisStore) set(conn redis.Conn, key string, value string, expire int64) error {
-	command, args := getSetCommandAndArgs(key, value, expire)
-	if _, err := conn.Do(command, args...); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func getSetCommandAndArgs(key string, value string, expire int64) (string, []interface{}) {
-	var args []interface{}
-	if expire == 0 {
-		args = append(args, key)
-		args = append(args, value)
-		return "SET", args
-	}
-
-	args = append(args, key)
-	args = append(args, expire)
-	args = append(args, value)
-	return "SETEX", args
 }
