c009780a89d7f799563bf7fd301754b6b15f127d
alileza <alirezayahya@gmail.com>
2017-10-24 15:08:57 +0200

    improvement to make sure that admin & startup event ready

--- pkg/plugin/prometheus/setup.go
+++ pkg/plugin/prometheus/setup.go
@@ -3,19 +3,25 @@ package prometheus
 import (
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/hellofresh/janus/pkg/errors"
 	"github.com/hellofresh/janus/pkg/plugin"
 	"github.com/hellofresh/janus/pkg/proxy"
-	"github.com/hellofresh/janus/pkg/router"
 	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
-	adminRouter router.Router
+	onStartupEventChan      chan *plugin.OnStartup
+	onAdminStartupEventChan chan *plugin.OnAdminAPIStartup
 )
 
 func init() {
+	onStartupEventChan = make(chan *plugin.OnStartup)
+	onAdminStartupEventChan = make(chan *plugin.OnAdminAPIStartup)
+
+	go onReady()
+
 	plugin.RegisterEventHook(plugin.StartupEvent, onStartup)
 	plugin.RegisterEventHook(plugin.AdminAPIStartupEvent, onAdminAPIStartup)
 
@@ -24,17 +30,32 @@ func init() {
 	})
 }
 
+func onReady() {
+	var (
+		startupEvent      *plugin.OnStartup
+		startupAdminEvent *plugin.OnAdminAPIStartup
+	)
+	for i := 0; i < 2; i++ {
+		select {
+		case startupEvent = <-onStartupEventChan:
+		case startupAdminEvent = <-onAdminStartupEventChan:
+		case <-time.NewTimer(time.Second * 5).C:
+			log.Errorf("prometheus plugin reach timeout of 5s waiting for startup & admin event")
+			return
+		}
+	}
+
+	if cfg := startupEvent.Config.Prometheus; cfg.Enabled {
+		startupAdminEvent.Router.GET(cfg.MetricPath, prometheus.Handler().ServeHTTP)
+	}
+}
+
 func onStartup(event interface{}) error {
 	e, ok := event.(plugin.OnStartup)
 	if !ok {
 		return fmt.Errorf("Could not convert event to startup type")
 	}
-	if adminRouter == nil {
-		return errors.New(http.StatusNotFound, "invalid admin router given")
-	}
-	if e.Config.Prometheus.Enabled {
-		adminRouter.GET(e.Config.Prometheus.MetricPath, prometheus.Handler().ServeHTTP)
-	}
+	onStartupEventChan <- &e
 	return nil
 }
 
@@ -43,7 +64,7 @@ func onAdminAPIStartup(event interface{}) error {
 	if !ok {
 		return fmt.Errorf("Could not convert event to admin startup type")
 	}
-	adminRouter = e.Router
+	onAdminStartupEventChan <- &e
 	return nil
 }
 
