574975a1f756e69ba1a7a327e3471f54aa43cbce
Sanjito Kurniawan <ssi@hellofresh.com>
2018-11-19 17:23:34 +0100

    Fix lint error

--- pkg/middleware/stats_tagger.go
+++ pkg/middleware/stats_tagger.go
@@ -6,19 +6,19 @@ import (
 	"go.opencensus.io/tag"
 )
 
-// statsTagger is a middleware that takes a list of tags and adds them into context to be propagated
-type statsTagger struct {
+// StatsTagger is a middleware that takes a list of tags and adds them into context to be propagated
+type StatsTagger struct {
 	tags []tag.Mutator
 }
 
-// NewStatsTagger creates a new instance of statsTagger
-func NewStatsTagger(tags []tag.Mutator) *statsTagger {
-	metricKeyInserter := &statsTagger{tags}
+// NewStatsTagger creates a new instance of StatsTagger
+func NewStatsTagger(tags []tag.Mutator) *StatsTagger {
+	metricKeyInserter := &StatsTagger{tags}
 	return metricKeyInserter
 }
 
 // Handler is the middleware function
-func (h *statsTagger) Handler(handler http.Handler) http.Handler {
+func (h *StatsTagger) Handler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
