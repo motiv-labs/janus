ad0c2aa183340097c806e10443c933fb35ee6bac
Illia Yalovoi <imperio091@gmail.com>
2023-10-20 11:35:58 +0300

    synced kafka code with lms

--- pkg/kafka/consumer.go
+++ pkg/kafka/consumer.go
@@ -2,6 +2,9 @@ package kafka
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/segmentio/kafka-go"
 
@@ -15,7 +18,7 @@ type FaultyKafkaMessageHandler func(Message, error)
 
 func ConsumeMessages(conf *Config, topic string, msgHandler KafkaMessageHandler, DLQHandler FaultyKafkaMessageHandler) {
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:           []string{conf.KafkaAddr},
+		Brokers:           conf.KafkaBrokers,
 		Topic:             topic,
 		GroupID:           conf.Consumer.Group,
 		QueueCapacity:     conf.Consumer.QueueCapacity,
@@ -24,21 +27,34 @@ func ConsumeMessages(conf *Config, topic string, msgHandler KafkaMessageHandler,
 		MaxWait:           conf.Consumer.MaxWait,
 		HeartbeatInterval: conf.Consumer.HeartbeatInterval,
 
-		GroupBalancers: []kafka.GroupBalancer{&kafka.RangeGroupBalancer{}},
+		GroupBalancers: []kafka.GroupBalancer{&kafka.RoundRobinGroupBalancer{}},
 	})
-	defer r.Close()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ch
+		if err := r.Close(); err != nil {
+			log.Printf("Cound not close kafka reader (Err: %s)\n", err)
+		}
+	}()
 
 	for {
-		m, err := r.ReadMessage(context.Background())
+		msg, err := r.FetchMessage(context.Background())
 		if err != nil {
 			log.Printf("Cound not read kafka message (Err: %s)\n", err.Error())
-			DLQHandler(m, err)
+			DLQHandler(msg, err)
 			continue
 		}
-		err = msgHandler(m)
+		err = msgHandler(msg)
 		if err != nil {
 			log.Printf("Cound not handle kafka message (Err: %s)\n", err.Error())
-			DLQHandler(m, err)
+			DLQHandler(msg, err)
+		}
+		err = r.CommitMessages(context.Background(), msg)
+		if err != nil {
+			log.Printf("Cound not commit kafka message (Err: %s)\n", err.Error())
+			DLQHandler(msg, err)
 		}
 	}
 }
